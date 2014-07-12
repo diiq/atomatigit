diiq/atomatigit

commit 15ae56d99d9da407fd48f5aa3eed14ef1def2842

Doubles quotes are evil :lipstick:
You must never go there, Simba. :see_no_evil:

lib/models/list-item.coffee
@@ -3,7 +3,7 @@
 ##
 # There are multiple places that atomatigit requires lists of items, navigable
 # by command/keybinding or by click. Some list items even have nested lists
-# inside them. It's navigation hell! Still, we can abstract out some of that
+# inside them. it is navigation hell! Still, we can abstract out some of that
 # into List and ListItem. Victory, bitter but wingèd.
 
 module.exports =
@@ -23,7 +23,7 @@ class ListItem extends Model
     @set selected: false
 
   selectedP: ->
-    @get "selected"
+    @get 'selected'
 
   allowPrevious: ->
     if @useSublist()

lib/models/repo.coffee
@@ -1,4 +1,5 @@
 _ = require 'underscore'
+path = require 'path'
 {Model} = require 'backbone'
 {File} = require 'pathwatcher'
 
@@ -31,7 +32,8 @@ class Repo extends Model
     @active_list.leaf()
 
   commitMessagePath: ->
-    atom.project.getRepo().getWorkingDirectory() + '/.git/COMMIT_EDITMSG_ATOMATIGIT'
+    cwd = atom.project.getRepo().getWorkingDirectory()
+    path.join cwd, '/.git/COMMIT_EDITMSG_ATOMATIGIT'
 
   headRefsCount: ->
     atom.project.getRepo().getReferences().heads.length
@@ -63,11 +65,11 @@ class Repo extends Model
       @writeCommitMessage(result)
 
   writeCommitMessage: (editor) =>
-    commitMessage = '\n' + """
+    commitMessage = '\n' + "
       # Please enter the commit message for your changes. Lines starting
       # with '#' will be ignored, and an empty message aborts the commit.
       # On branch #{@current_branch.localName()}\n
-    """
+    "
 
     filesStaged = @file_list.staged()
     filesUnstaged = @file_list.unstaged()
@@ -87,7 +89,7 @@ class Repo extends Model
     editor.setCursorBufferPosition [0, 0]
 
   completeCommit: ->
-    git.git "commit --cleanup=strip --file=\"#{@commitMessagePath()}\""
+    git.git "commit --cleanup=strip --file=\"#{@commitMessagePath()}\'"
     git.decrementTaskCounter()
 
   initiateCreateBranch: ->

lib/atomatigit.coffee
@@ -30,5 +30,3 @@ module.exports =
   deactivate: ->
     @repo_view.destroy()
     @repo.destroy()
-
-  serialize: ->

lib/models/branches/branch.coffee
@@ -7,18 +7,18 @@ class Branch extends ListItem
   name: ->
     # The name should be unicode-encoded. decode/escape repairs the
     # encoding.
-    decodeURIComponent escape @get "name"
+    decodeURIComponent escape @get 'name'
 
   localName: ->
     @name()
 
   head: ->
-    @get("commit").id
+    @get('commit').id
 
   commit: ->
-    new Commit @get "commit"
+    new Commit @get 'commit'
 
-  remoteName: -> ""
+  remoteName: -> ''
 
   unpushed: -> false
 
@@ -26,13 +26,13 @@ class Branch extends ListItem
     atom.confirm
       message: "Delete branch #{@name()}?"
       buttons:
-        "Delete": @delete
-        "Cancel": null
+        'Delete': @delete
+        'Cancel': null
 
   open: ->
     @checkout()
 
-  checkout: (callback)->
+  checkout: (callback) ->
     git.git "checkout #{@localName()}"
 
   push: -> null

lib/models/branches/current-branch.coffee
@@ -5,17 +5,17 @@ LocalBranch = require './local-branch'
 module.exports =
 class CurrentBranch extends LocalBranch
   initialize: (branchExisting) ->
-    if branchExisting then @reload()
+    @reload() if branchExisting
 
   reload: ->
     git.branch (head) =>
       @set head
 
     # git.gitNoChange "log @{u}..", (output) =>
-    #   @set unpushed: (output != "")
+    #   @set unpushed: (output != '')
 
   head: ->
-    "HEAD"
+    'HEAD'
 
   delete: -> null
 

lib/models/branches/index.coffee
@@ -1,6 +1,6 @@
 module.exports =
-  Branch: require "./branch"
-  LocalBranch: require "./local-branch"
-  RemoteBranch: require "./remote-branch"
-  CurrentBranch: require "./current-branch"
-  BranchList: require "./branch-list"
+  Branch: require './branch'
+  LocalBranch: require './local-branch'
+  RemoteBranch: require './remote-branch'
+  CurrentBranch: require './current-branch'
+  BranchList: require './branch-list'

lib/models/branches/local-branch.coffee
@@ -9,13 +9,13 @@ class LocalBranch extends Branch
   local: true
 
   unpushed: ->
-    @get "unpushed"
+    @get 'unpushed'
 
   delete: ->
     git.git "branch -D #{@name()}"
 
   # TODO tracking branch or something?
-  remoteName: -> ""
+  remoteName: -> ''
 
   checkout: (callback) ->
     git.git "checkout #{@localName()}"

lib/models/branches/remote-branch.coffee
@@ -11,7 +11,7 @@ class RemoteBranch extends Branch
     git.git "push -f #{@remoteName()} :#{@localName()}"
 
   localName: ->
-    @name().replace /.*?\//, ""
+    @name().replace /.*?\//, ''
 
   remoteName: ->
-    @name().replace /\/.*/, ""
+    @name().replace /\/.*/, ''

commit bb09528cf11938dfa4abef83b83a1ef307bbd865

Fix errors introduced by 15ae56d99d9da407fd48f5aa3eed14ef1def2842

lib/models/list-item.coffee
@@ -3,7 +3,7 @@
 ##
 # There are multiple places that atomatigit requires lists of items, navigable
 # by command/keybinding or by click. Some list items even have nested lists
-# inside them. it is navigation hell! Still, we can abstract out some of that
+# inside them. It's navigation hell! Still, we can abstract out some of that
 # into List and ListItem. Victory, bitter but wingèd.
 
 module.exports =

lib/models/repo.coffee
@@ -65,11 +65,11 @@ class Repo extends Model
       @writeCommitMessage(result)
 
   writeCommitMessage: (editor) =>
-    commitMessage = '\n' + "
+    commitMessage = '\n' + """
       # Please enter the commit message for your changes. Lines starting
       # with '#' will be ignored, and an empty message aborts the commit.
       # On branch #{@current_branch.localName()}\n
-    "
+    """
 
     filesStaged = @file_list.staged()
     filesUnstaged = @file_list.unstaged()
@@ -89,7 +89,7 @@ class Repo extends Model
     editor.setCursorBufferPosition [0, 0]
 
   completeCommit: ->
-    git.git "commit --cleanup=strip --file=\"#{@commitMessagePath()}\'"
+    git.git "commit --cleanup=strip --file=\"#{@commitMessagePath()}\""
     git.decrementTaskCounter()
 
   initiateCreateBranch: ->